c954995a8b9eb86aff5ad1e5456c348df8765718
Karina Gonzalez <karinagonzalez@Karinas-MacBook-Pro.local>
2024-06-06 20:19:41 -0600

    files and server started

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-// SERVER.JS
+// dependencies
+const express = require('express');
 
+// set up express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up routes
+app.use(require(''))
+
+// start server
+app.listen(PORT, () => {
+    console.log('Server listening on http://localhost:' + PORT);
+})
