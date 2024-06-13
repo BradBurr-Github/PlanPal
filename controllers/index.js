7a043b9b1ce4ad268fe63c1077f8c080a19e6298
BradBurr-GH <45184565+BradBurr-Github@users.noreply.github.com>
2024-06-12 21:35:17 -0600

    resolved conflicts

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 // Import the index.js from 'api' folder
 const apiRoutes = require('./api');
-const homeRoutes = require('./api/homeRoutes');
+const homeRoutes = require('./homeRoutes');
 const userRoutes = require('./api/userRoutes');
 
 router.use('/api', apiRoutes);
