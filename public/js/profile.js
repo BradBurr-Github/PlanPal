5a2fc12d112b5f150ca3fc9b86aaae76d92a6cfa
BradBurr-GH <45184565+BradBurr-Github@users.noreply.github.com>
2024-06-12 21:22:11 -0600

    merge conflicts

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-
-
-console.log('TEST-TEST-TEST-TEST-TEST-TEST-TEST-TEST-TEST-');
-
-=======
 //fetch call to our events api route save the data as our events array
 
 console.log("hello profile.js")
->>>>>>> main
 
 let ec = new EventCalendar(document.getElementById('ec'), {
     view: 'timeGridWeek',
     events: [
-<<<<<<< HEAD
-        // your list of events
-    ]
-    });
-=======
         // your list of events array
         ]
 
         });
 
     // console.log ("howdy")
->>>>>>> main
